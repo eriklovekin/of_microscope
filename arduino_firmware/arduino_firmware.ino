c04a34ef8a41564bf4337cabd35cccf1e76cc0f6
eriklovekin <elovekin@ucsd.edu>
2023-09-03 15:28:17 -0700

    basic button layout and xy motor control from gui

--- arduino_firmware/arduino_firmware.ino
+++ arduino_firmware/arduino_firmware.ino
@@ -4,18 +4,19 @@
 const int spr = 64;//Steps Per Revolution
 
 // Initialize Stepper Motors
-Stepper x_motor(spr,2,3,4,5);
-Stepper y_motor(spr,6,7,8,9);
-Stepper z_motor(spr,10,11,12,13);
+Stepper x_motor(spr,2,4,3,5);
+Stepper y_motor(spr,6,8,7,9);
+Stepper z_motor(spr,10,12,11,13);
 
 void setup() {
   Serial.begin(9600);
-  Serial.print('Arduino Connected');
+  delay(2000);
+  Serial.println("Arduino Connected!");
 
   //Set motor speeds
   x_motor.setSpeed(200);
-  y_motor.setSpeed(3);
-  z_motor.setSpeed(3);
+  y_motor.setSpeed(200);
+  z_motor.setSpeed(200);
 
 
 }
@@ -23,8 +24,28 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
-    int command = Serial.read();
-    Serial.print(command, DEC);
+    String command = Serial.readString();
+    Serial.println(command);
+    if(command.equals("x_pos_btn")){
+      Serial.println("X+ button pressed");
+      x_motor.step(500);
+    }
+    else if(command.equals("x_neg_btn")){
+      Serial.println("X- button pressed");
+      x_motor.step(-500);
+    }
+    else if(command.equals("y_pos_btn")){
+      Serial.println("Y+ button pressed");
+      y_motor.step(500);
+    }
+    else if(command.equals("y_neg_btn")){
+      Serial.println("Y- button pressed");
+      y_motor.step(-500);
+    }
+      
+      
+      
+    
   }
   // x_motor.step(3000);
   // delay(1000);
--- microscope_gui.py
+++ microscope_gui.py
@@ -6,66 +6,67 @@ import serial
 import pygame
 
 # # Initialize PySerial for Arduino communication
-arduino = serial.Serial('/dev/ttyUSB0', 9600)  # Replace with the correct port
+arduino = serial.Serial('/dev/ttyUSB1', 9600)  # Replace with the correct port
 
 # # Initialize Pygame for keystrokes
 # pygame.init()
 
 # Initialize OpenCV for camera
-# cap = cv2.VideoCapture(0)  # Use the appropriate camera index
+cap = cv2.VideoCapture(0)  # Use the appropriate camera index
 
 
-
+img_span_row = int(10)
+img_span_col = int(10)
 # # Function to send commands to Arduino
 def send_command(command):
     arduino.write(command.encode())
+    print(command)
 
 # # Function to capture an image
-# def capture_image():
-#     ret, frame = cap.read()
-#     if ret:
-#         cv2.imwrite('captured_image.jpg', frame)
+def capture_image():
+    ret, frame = cap.read()
+    if ret:
+        cv2.imwrite('captured_image.jpg', frame)
 
 # Create the GUI
 root = tk.Tk()
 root.title("OpenFlexure Microscope")
+root.geometry("450x350")
 
 # # Motor control buttons
-x1_button = tk.Button(root, text="X+", command=lambda: send_command("1"))
-# x2_button = tk.Button(root, text="X-", command=lambda: send_command("x2"))
-# y1_button = tk.Button(root, text="Y+", command=lambda: send_command("y1"))
-# y2_button = tk.Button(root, text="Y-", command=lambda: send_command("y2"))
-# z1_button = tk.Button(root, text="IN", command=lambda: send_command("z1"))
-# z2_button = tk.Button(root, text="OUT", command=lambda: send_command("z2"))
+x_pos_btn = tk.Button(root, text="X+", command=lambda: send_command("x_pos_btn"),bg="RED",fg="WHITE")
+x_neg_btn = tk.Button(root, text="X-", command=lambda: send_command("x_neg_btn"),bg="RED",fg="WHITE")
+y_pos_btn = tk.Button(root, text="Y+", command=lambda: send_command("y_pos_btn"),bg="GREEN",fg="WHITE")
+y_neg_btn = tk.Button(root, text="Y-", command=lambda: send_command("y_neg_btn"),bg="GREEN",fg="WHITE")
+z_out_btn = tk.Button(root, text="OUT", command=lambda: send_command("z_out_btn"),bg="BLUE",fg="WHITE")
+z_in_btn = tk.Button(root, text="IN", command=lambda: send_command("z_in_btn"),bg="BLUE",fg="WHITE")
 
-x1_button.pack()
-# x2_button.pack()
-# y1_button.pack()
-# y2_button.pack()
-# z1_button.pack()
-# z2_button.pack()
-# motor1_button.pack()
-# motor2_button.pack()
-# motor3_button.pack()
+# # Camera Buttons
+snap_btn = tk.Button(root, text="Capture Image", command=capture_image)
 
-# # Image capture button
-# capture_button = tk.Button(root, text="Capture Image", command=capture_image)
-# capture_button.pack()
+# # Button Positioning
+x_pos_btn.grid(row=3,column=5+img_span_col,columnspan=2,sticky='EW')
+x_neg_btn.grid(row=3,column=1+img_span_col,columnspan=2,sticky='EW')
+y_pos_btn.grid(row=2,column=3+img_span_col,columnspan=2,sticky='EW')
+y_neg_btn.grid(row=4,column=3+img_span_col,columnspan=2,sticky='EW')
+z_out_btn.grid(row=1,column=4+img_span_col,columnspan=3,sticky='EW')
+z_in_btn.grid(row=1,column=1+img_span_col,columnspan=3,sticky='EW')
+snap_btn.grid(row=5,column=1+img_span_col,columnspan=6,sticky='EW')
 
 # Create a label for displaying the camera feed
-# camera_label = tk.Label(root)
-# camera_label.pack()
+camera_label = tk.Label(root)
+camera_label.grid(row=1,column=1,rowspan=img_span_row,columnspan=img_span_col,sticky='NESW')
 
 # Main loop to update the camera feed
-# def update_camera():
-#     ret, frame = cap.read()
-#     if ret:
-#         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         img = cv2.resize(frame, (640, 480))
-#         img = tk.PhotoImage(data=img.tostring())
-#         camera_label.config(image=img)
-#         camera_label.img = img
-#         root.after(10, update_camera)  # Update every 10ms
+def update_camera():
+    ret, frame = cap.read()
+    if ret:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(frame, (640, 480))
+        img = tk.PhotoImage(data=img.tostring())
+        camera_label.config(image=img)
+        camera_label.img = img
+        root.after(10, update_camera)  # Update every 10ms
 
 # update_camera()
 
